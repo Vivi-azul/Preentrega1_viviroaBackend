6904585bcbf752173cc9f202d91e41e4a9777303
Viviana Roa <viroa.rodriguez@gmail.com>
2024-06-10 21:49:06 -0400

    reparando errores de syntaxis

--- src/rutas/products.js
+++ src/rutas/products.js
@@ -3,125 +3,124 @@ import fs from "fs";
 import path from "path";
 
 const router = express.Router();
-const productsFilePath = path.resolve("./src/data/products.json");
+const productosFilePath = path.resolve("./src/data/productos.json");
 
-const getProducts = () => {
+const getProductos = () => {
   try {
-    const data = fs.readFileSync(productsFilePath, "utf-8");
+    const data = fs.readFileSync(productosFilePath, "utf-8");
     return JSON.parse(data) || [];
   } catch (error) {
-    console.error("Error reading products file:", error);
+    console.error("Error al leer el Producto del archivo:", error);
     return [];
   }
 };
 
-const saveProducts = (products) => {
-  fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+const guardarProductos = (products) => {
+  fs.writeFileSync(productosFilePath, JSON.stringify(productos, null, 2));
 };
 
 
 //Obtener todos los productos desde products.json o limitando con el valor obtenido desde el req.query
 router.get("/", (req, res) => {
   const { limit } = req.query;
-  const products = getProducts();
+  const products = getProductos();
   if (limit) {
-    res.json(products.slice(0, limit));
+    res.json(productos.slice(0, limit));
   } else {
-    res.json(products);
+    res.json(productos);
   }
 });
 
 //Obtener producto segun el ID indicado
 router.get("/:pid", (req, res) => {
-  const products = getProducts();
-  const product = products.find((p) => p.id === req.params.pid);
-  if (product) {
-    res.json(product);
+  const productos = getProducts();
+  const producto = productos.find((p) => p.id === req.params.pid);
+  if (producto) {
+    res.json(producto);
   } else {
     res
       .status(404)
-      .json({ message: `Product with ID ${req.params.pid} was not found` });
+      .json({ mensaje: `Producto con ID ${req.params.pid} no fue encontrardo` });
   }
 });
 
 // //Crear producto validando que se ingresen los datos requeridos
-// router.post("/", (req, res) => {
-//   const {
-//     title,
-//     description,
-//     code,
-//     price,
-//     status = true,
-//     stock,
-//     category,
-//     thumbnails = [],
-//   } = req.body;
-//   if (!title || !description || !code || !price || !stock || !category) {
-//     return res
-//       .status(400)
-//       .json({ message: "All fields except thumbnails are required" });
-//   }
+router.post("/", (req, res) => {
+   const {
+     title,
+     description,
+     code,
+     price,
+     status = true,
+     stock,
+     category,  
+   } = req.body;
+   if (!title || !description || !code || !price || !stock || !category) {
+     return res
+       .status(400)
+       .json({ mensaje: "Todos los campos son obligatorios" });
+   }
 
-//   const products = getProducts();
-//   const id =
-//     (products.length ? Math.max(...products.map((p) => parseInt(p.id))) : 0) +
-//     1;
-//   const newProduct = {
-//     id: id.toString(),
-//     title,
-//     description,
-//     code,
-//     price,
-//     status,
-//     stock,
-//     category,
-//     thumbnails,
-//   };
-//   products.push(newProduct);
-//   saveProducts(products);
-//   res.status(201).json({
-//     message: "Product successfully created",
-//     newProduct,
-//   });
-// });
+   const productos = getProductos();
+   const id =
+     (products.length ? Math.max(...products.map((p) => parseInt(p.id))) : 0) +
+     1;
+   const newProduct = {
+     id: id.toString(),
+     title,
+     description,
+     code,
+     price,
+     status,
+     stock,
+     category,
+     thumbnails,
+   };
+   products.push(newProduct);
+   saveProducts(products);
+   res.status(201).json({
+     mensaje: "Producto creado exitosamente",
+     nuevoProducto,
+   });
+ });
 
-// //Actualizar producto segun el ID indicado
-// router.put("/:pid", (req, res) => {
-//   const products = getProducts();
-//   const productIndex = products.findIndex((p) => p.id === req.params.pid);
-//   if (productIndex === -1) {
-//     return res
-//       .status(404)
-//       .json({ message: `Product with ID ${req.params.pid} was not found` });
-//   }
+//Actualizar producto segun el ID indicado
+ router.put("/:pid", (req, res) => {
+   const products = getProducts();
+   const productIndex = products.findIndex((p) => p.id === req.params.pid);
+   if (productIndex === -1) {
+     return res
+       .status(404)
+       .json({ mensaje: `Producto con ID ${req.params.pid} no fue encontrado` });
+   }
 
-//   const updatedProduct = {
-//     ...products[productIndex],
-//     ...req.body,
-//     id: products[productIndex].id,
-//   };
-//   products[productIndex] = updatedProduct;
-//   saveProducts(products);
-//   res.json({ message: "Product successfully updated", updatedProduct });
-// });
+   const productoActualizado = {
+     ...products[productIndex],
+     ...req.body,
+     id: products[productIndex].id,
+   };
+   products[productIndex] = productoActualizado;
+   guardarProductos(products);
+   res.json({ message: "Producto actualizado con exito", productoActualziado });
+ });
 
-// //Eliminar producto segun el ID indicado
-// router.delete("/:pid", (req, res) => {
-//   const products = getProducts();
-//   const newProducts = products.filter((p) => p.id !== req.params.pid);
+ //Eliminar producto segun el ID indicado
+ router.delete("/:pid", (req, res) => {
+   const products = getProducts();
+   const newProducts = products.filter((p) => p.id !== req.params.pid);
 
-//   if (newProducts.length === products.length) {
-//     return res
-//       .status(404)
-//       .json({ message: `Product with ID ${req.params.pid} was not found` });
-//   }
+   if (newProduct.length === products.length) {
+     return res
+       .status(404)
+       .json({ mensaje: `Producto con ID ${req.params.pid} no fue encontrado` });
+   }
 
-//   saveProducts(newProducts);
-//   res
-//     .status(200)
-//     .json({
-//       message: `Product with ID ${req.params.pid} successfully deleted`,
-//     });
-// });
+   guardarProductos(nuevoProductos);
+   res
+     .status(200)
+     .json({
+       mensaje: `Producto con ID ${req.params.pid} exitosamente eliminado`,
+     });
+ });
 
 export default router;
